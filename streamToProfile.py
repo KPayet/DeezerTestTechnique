13ac2993a6ff42c029ece64fafa1871e26dd51ba
Ubuntu <ubuntu@ip-172-31-34-152.eu-west-1.compute.internal>
2015-08-05 18:27:45 +0000

    only minor changes left to add, and it will be ready

--- streamToProfile.py
+++ streamToProfile.py
@@ -1,16 +1,21 @@
+import os
 from pyspark import SparkContext
+
+def saveAsTextFile(rdd, path, overwrite=False):
+    if overwrite:
+        os.system("rm -r " + path)
+
+    rdd.saveAsTextFile(path, compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
 sc = SparkContext("local", "usersProfile")
 
-rawData = sc.textFile("./fakeData*", 22)
+rawData = sc.textFile("./fakeData1M", 4)
 
-parsedData = (rawData.map(lambda line: line.split("|"))
-		     .map(lambda s: ((int(s[0]), int(s[3])), 1))) # UserID, TrackId, 1
-					
-# then we reduce by key (sum all ones, by grouping by (UserID, TrackId) key
+outData = (rawData.map(lambda line: line.split("|"))
+		     .filter(lambda s: int(s[0])<=10000)
+		     .map(lambda t: ((int(t[0]), int(t[3])), 1))
+		     .reduceByKey(lambda a,b: a+b)
+		     .sortByKey()
+		     .map(lambda t: " ".join(map(str, (t[0][0], t[0][1], t[1])))))
 
-redData = (parsedData.reduceByKey(lambda a,b: a+b)
-		     .sortByKey())
-
-redData.saveAsHadoopFile("profileMat", "org.apache.hadoop.mapred.TextOutputFormat",
-                          compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
+saveAsTextFile(outData, path = "./profileMat", overwrite = True)
