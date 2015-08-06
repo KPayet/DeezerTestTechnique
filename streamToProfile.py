535ff278e004b868ec05e7d63a790f8a3a8e1ff2
Ubuntu <ubuntu@ip-172-31-1-190.eu-west-1.compute.internal>
2015-08-06 09:02:44 +0000

    working version of script to turn raw stream_xxxxxx file to utility matrix in coordinate list format

--- streamToProfile.py
+++ streamToProfile.py
@@ -1,3 +1,5 @@
+import sys
+import getopt
 import os
 from pyspark import SparkContext
 
@@ -7,15 +9,45 @@ def saveAsTextFile(rdd, path, overwrite=False):
 
     rdd.saveAsTextFile(path, compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
-sc = SparkContext("local", "usersProfile")
+def getArguments(argv):
+    
+    inputFile = ""
+    outputFile = ""
+    nPart = 4
+    
+    try:
+      options, args = getopt.getopt(argv,"hi:o:n:")
+    except getopt.GetoptError:
+      print 'streamToProfile.py -i <inputfile> -o <outputfile> -n <npartitions>'
+      sys.exit(2)
+    for option, arg in options:
+      if option == '-h':
+         print 'streamToProfile.py -i <inputfile> -o <outputfile> -n <npartitions>'
+         sys.exit()
+      elif option == "-i":
+         inputFile = arg
+      elif option == "-o":
+         outputFile = arg
+      elif option == "-n":
+         nPart = int(arg)
+      
+    return inputFile, outputFile, nPart
 
-rawData = sc.textFile("./fakeData1M", 4)
 
-outData = (rawData.map(lambda line: line.split("|"))
-		     .filter(lambda s: int(s[0])<=10000)
-		     .map(lambda t: ((int(t[0]), int(t[3])), 1))
-		     .reduceByKey(lambda a,b: a+b)
-		     .sortByKey()
-		     .map(lambda t: " ".join(map(str, (t[0][0], t[0][1], t[1])))))
+def main(argv):
 
-saveAsTextFile(outData, path = "./profileMat", overwrite = True)
+    inputFile, outputFile, nPart = getArguments(argv)
+
+    sc = SparkContext(appName="usersProfile")
+    rawData = sc.textFile(inputFile, nPart)
+
+    outData = (rawData.map(lambda line: line.split("|"))
+		      .map(lambda t: ((int(t[0]), int(t[3])), 1))
+		      .reduceByKey(lambda a,b: a+b)
+		      .sortByKey()
+		      .map(lambda t: " ".join(map(str, (t[0][0], t[0][1], t[1])))))
+
+    saveAsTextFile(outData, path = outputFile, overwrite = True)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
