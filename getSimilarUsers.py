38445f77d0afa1af4aa2b50ee4fc8ade180b2a54
Ubuntu <ubuntu@ip-172-31-2-114.eu-west-1.compute.internal>
2015-08-07 17:20:40 +0000

    some cosmetics

--- getSimilarUsers.py
+++ getSimilarUsers.py
@@ -8,7 +8,7 @@ def getSimilarUsers(userId, rdd):
    
     top20 = (rdd.map(lambda line: line.split(" "))  # parse from line to tuple
                 .map(lambda t: (int(t[0]), int(t[1]), float(t[2])))
-		        .filter(lambda t: t[0] == userId)       #t[0] = user1, t[1] = user2, t[2] = sim(user1, user2)
+		.filter(lambda t: t[0] == userId)       #t[0] = user1, t[1] = user2, t[2] = sim(user1, user2)
                 .sortBy(lambda t: -1*t[2])          # we sort by descending order of similarity (t[2])
                 .map(lambda t: t[1])
                 .take(20)) # return as a list
