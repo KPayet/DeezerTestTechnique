0d6a8c88ebb3f7666ccc2484d301603a10c4e26a
Kevin Payet <k_payet@yahoo.fr>
2015-08-07 17:38:52 +0200

    added python script for question 2.c

---
+++ getSimilarUsers.py
@@ -0,0 +1,45 @@
+import sys
+import getopt
+from pyspark import SparkContext
+
+def getArguments(argv):
+    
+    userId = 1
+
+    try:
+      options, args = getopt.getopt(argv,"hu:")
+    except getopt.GetoptError:
+      print 'streamToProfile.py -u <user_id>'
+      sys.exit(2)
+    for option, arg in options:
+      if option == '-h':
+         print 'streamToProfile.py -u <user_id>'
+         sys.exit()
+      elif option == "-u":
+         userId = arg
+
+    return userId
+
+# la 
+def getSimilarUsers(id, rdd):
+    
+    top20 = (rdd.map(lambda line: line.split(" "))  # parse from line to tuple
+                .filter(lambda t: t[0] == id)       #t[0] = user1, t[1] = user2, t[2] = sim(user1, user2)
+                .sortBy(lambda t: -1*t[2])          # we sort by descending order of similarity (t[2])
+                .map(lambda t: t[1])
+                .take(20)) # return as a list
+                
+    return top20
+    
+if __name__ == "__main__":
+    
+    id = getArguments(argv)
+    
+    # init Spark, and retrieve similarityMatrix.
+    # I suppose this one is stored on hdfs, and compressed.
+    sc = SparkContext("top20Similarusers")
+    simMatrix = sc.textFile("hdfs:///deezer/similarityMatrix/part*.gz")
+    
+    top20 = getSimilarUsers(id, simMatrix)
+    
+    print "The 20 users most similar to user ", id, " are: ", top20"
