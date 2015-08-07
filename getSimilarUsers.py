89099bf39b7f7bfdb1f66f35fcd6a43c43609d0c
Kevin Payet <k_payet@yahoo.fr>
2015-08-07 19:17:51 +0200

    added some modifs and comments to getUsersSimilarity

--- getSimilarUsers.py
+++ getSimilarUsers.py
@@ -2,10 +2,23 @@ import sys
 import getopt
 from pyspark import SparkContext
 
-def getArguments(argv):
+
+# The function takes as argument the userId, and an RDD containing the similarity matrix 
+def getSimilarUsers(userId, rdd):
+   
+    top20 = (rdd.map(lambda line: line.split(" "))  # parse from line to tuple
+                .map(lambda t: (int(t[0]), int(t[1]), float(t[2])))
+		        .filter(lambda t: t[0] == userId)       #t[0] = user1, t[1] = user2, t[2] = sim(user1, user2)
+                .sortBy(lambda t: -1*t[2])          # we sort by descending order of similarity (t[2])
+                .map(lambda t: t[1])
+                .take(20)) # return as a list
+                
+    return top20
+
+def main(argv):
     
     userId = 1
-
+    
     try:
       options, args = getopt.getopt(argv,"hu:")
     except getopt.GetoptError:
@@ -17,28 +30,7 @@ def getArguments(argv):
          sys.exit()
       elif option == "-u":
          userId = int(arg)
-
-    return userId
-
-def test(user1, userId):
-   return user1==userId
-
-# la 
-def getSimilarUsers(userId, rdd):
-   
-    top20 = (rdd.map(lambda line: line.split(" "))  # parse from line to tuple
-                .map(lambda t: (int(t[0]), int(t[1]), float(t[2])))
-		.filter(lambda t: t[0] == userId)       #t[0] = user1, t[1] = user2, t[2] = sim(user1, user2)
-                .sortBy(lambda t: -1*t[2])          # we sort by descending order of similarity (t[2])
-                .map(lambda t: t[1])
-                .take(20)) # return as a list
-                
-    return top20
-    
-if __name__ == "__main__":
-    
-    userId = getArguments(sys.argv[1:])
-    
+         
     # init Spark, and retrieve similarityMatrix.
     # I suppose this one is stored on hdfs, and compressed.
     sc = SparkContext(appName = "top20Similarusers")
@@ -47,3 +39,9 @@ if __name__ == "__main__":
     top20 = getSimilarUsers(userId, simMatrix)
     
     print "\n The 20 users most similar to user ", userId, " are: ", top20, "\n"
+
+
+if __name__ == "__main__":
+    
+    main(sys.argv[1:])
+    
